4c8758ab49ce44f32660e4e5d43f9678fa670a97
Hossein Najafi <hossei.najafi138@gmail.com>
2026-08-02 15:29:45 +0330

    Update normalization.py

--- NSGAIII/normalization.py
+++ NSGAIII/normalization.py
@@ -1,7 +1,7 @@
 
 # here the  first method of Investigating the Normalization Procedure of NSGA-III is implemented https://doi.org/10.1007/978-3-030-12598-1_19 
 #  if you prefer the other methods can implement the others too 
-def normalize(sortedPop,pop):
+def normalize(sortedPop,pop,lf):
     eps = 1e-4
     nadirPoint = [0] * len(sortedPop[0][0]) 
     idealPoint = [0] * len(sortedPop[0][0]) 
@@ -9,30 +9,35 @@ def normalize(sortedPop,pop):
     for s in sortedPop[0]:
         i = 0
         while i < len(s):
-            if nadirPoint[i] < s[i] or flag == 1:
-                nadirPoint[i] = s[i]
+            if idealPoint[i] > s[i] or flag == 1:
+                idealPoint[i] = s[i]
             i += 1
         flag = 0
 
     i = 0
-    flag1 = [1] * len(sortedPop[0][0]) 
+    flag1 = 1 
 
-    while i < len(sortedPop[i]):
+    while i < len(sortedPop):
         j = 0
-        while j < len(sortedPop[i][j]) or flag1[j] == 1:
-            if idealPoint[i] > sortedPop[i][j] or flag1[j] == 1:
-                idealPoint = sortedPop[i][j]
-                flag1[j] = 0
-            
+        while j < len(sortedPop[i]) :
+            k = 0
+            while k < len(nadirPoint):
+                if nadirPoint[k] < sortedPop[i][j][k] or flag1 == 1:
+                    nadirPoint[k] = sortedPop[i][j][k]
+    
+
+            flag1 = 0
             j += 1
+
+        
 
         flag = 0
         j = 0
         flag2 = True
-        while j < len(idealPoint): 
-            if nadirPoint - idealPoint < eps:
-                flag[i] = 1
+        while j < len(nadirPoint): 
+            if nadirPoint[j] - idealPoint[j] < eps:
                 flag2 = False
+                
 
         if flag2 :
             break
@@ -44,12 +49,22 @@ def normalize(sortedPop,pop):
     while i < len(pop):
         j = 0
         while j < len(pop[0]):
-            pop[i][j] = pop[i][j] - idealPoint[j] / nadirPoint[j] - idealPoint[j] 
+            pop[i][j] = (pop[i][j] - idealPoint[j]) / (nadirPoint[j] - idealPoint[j] )
             j += 1   
 
         i += 1
 
+    i = 0 
+    q =[]
+    while i < len(sortedPop[lf]):
+        j = 0
+        z =[]
+        while j < len(pop[0]):
+            z.append(( sortedPop[lf][i][j] - idealPoint[j] )/ (nadirPoint[j] - idealPoint[j])) 
+            j += 1   
+        q.append(z)
+        i += 1
 
-
-
+    
+    return pop , q
 

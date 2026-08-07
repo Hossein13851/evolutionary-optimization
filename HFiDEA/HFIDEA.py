d595d1a1b891a1a34b7f0190c6d5ef41df088a2f
Hossein Najafi <hossei.najafi138@gmail.com>
2026-08-07 23:29:45 +0330

    Add files via upload

---
+++ HFiDEA/HFIDEA.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+def objFunction():
+    pass
+
+def generateInitialSolutions():
+    pass
+
+
+def selectEnvironment():
+    
+
+def checkStabalization():
+    pass
+
+def updateNadirP():
+    pass
+
+def updateNadir():
+    pass
+
+def HFIDEA():
+    pass    
+
